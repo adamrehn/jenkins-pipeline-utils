6a024f3e3dcbaab1ebe936965fa7aaec83426f4d
Adam Rehn <adam.rehn@my.jcu.edu.au>
2018-05-03 21:33:28 +1000

    Initial source commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# jenkins-pipeline-utils
-Helper code for use in Jenkins Declarative Pipelines
+# Jenkins Pipeline Utils
+
+This repository contains small snippets of code for use in [Jenkins Declarative Pipelines](https://jenkins.io/doc/book/pipeline/).
+
+To dynamically load this library in a Pipeline without the need to preconfigure it in Jenkins, use the following code:
+
+```groovy
+library identifier: 'jenkins-pipeline-utils@master', retriever: modernSCM([$class: 'GitSCMSource', remote: 'https://github.com/adamrehn/jenkins-pipeline-utils.git'])
+```
---
+++ vars/windowsContainer.groovy
@@ -0,0 +1,26 @@
+//Runs the supplied list of commands in the specified Windows container image
+//(We need to implement this ourselves since Jenkins is yet to implement support for Windows containers)
+def call(image, commands)
+{
+	//Store all environment variables starting with JENKINS_ in a file so they can be forwarded to the container
+	bat 'set JENKINS_ > env.vars'
+	
+	//Since compiling code in a Windows container bind-mount seems to have issues,
+	//we need to copy the workspace to another directory and run the commands there,
+	//copying back any generated files once the commands have completed.
+	def targetDir = 'C:\\workspace'
+	def batchCommands = [
+		"@echo off",
+		"xcopy \"%WORKSPACE%\" \"${targetDir}\" /S /I /q",
+		"cd \"${targetDir}\""
+	] + commands + [
+		"xcopy \"${targetDir}\" \"%WORKSPACE%\" /S /y /q"
+	]
+	
+	//Run the container
+	writeFile(file: '_______jenkins_entrypoint.bat', text: batchCommands.join('\r\n'))
+	bat 'docker run --rm ' +
+		'-e "WORKSPACE=%WORKSPACE%" --env-file env.vars ' +
+		'-v "%WORKSPACE%:%WORKSPACE%" -w "%WORKSPACE%" ' +
+		"\"${image}\" cmd /S /C _______jenkins_entrypoint.bat"
+}
